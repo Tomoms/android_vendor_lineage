70246d1a9404f2fbf24b12a0090fdf593afa3038
Tomoms <fondatommaso2@gmail.com>
2019-04-12 23:09:46 +0200

    update sdclang pth
    Change-Id: Ib635756fce238ee8acc3e46d4a57419b81ae23ac

--- sdclang/vendorsetup.sh
+++ sdclang/vendorsetup.sh
@@ -1,6 +1,6 @@
 # Set SDClang defaults
 export SDCLANG=false
-export SDCLANG_PATH=vendor/qcom/sdclang/bin
+export SDCLANG_PATH=prebuilts/snapdragon-llvm-4.0.14-linux64/toolchains/llvm-Snapdragon_LLVM_for_Android_4.0/prebuilt/linux-x86_64/bin
 export SDCLANG_LTO_DEFS=vendor/lineage/sdclang/sdllvm-lto-defs.mk
 export SDCLANG_COMMON_FLAGS="-O3 -g0 -mcpu=krait -Wno-user-defined-warnings -Wno-vectorizer-no-neon -Wno-unknown-warning-option \
 -Wno-deprecated-register -Wno-tautological-type-limit-compare -Wno-sign-compare -Wno-gnu-folding-constant \
