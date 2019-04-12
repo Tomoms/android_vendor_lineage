8393281cdbd3b27a61789b2bcf86c46eb0afd844
Tomoms <fondatommaso2@gmail.com>
2019-04-12 22:00:01 +0200

    sdclang: correct path and flags for Krait targets
    Change-Id: Ib4568be343c1d3a46a929143164378e9ca9db2a4

--- sdclang/vendorsetup.sh
+++ sdclang/vendorsetup.sh
@@ -1,8 +1,8 @@
 # Set SDClang defaults
 export SDCLANG=false
 export SDCLANG_PATH=vendor/qcom/sdclang/bin
-export SDCLANG_LTO_DEFS=vendor/xtended/sdclang/sdllvm-lto-defs.mk
-export SDCLANG_COMMON_FLAGS="-Ofast -g0 -mcpu=cortex-a57 -Wno-user-defined-warnings -Wno-vectorizer-no-neon -Wno-unknown-warning-option \
+export SDCLANG_LTO_DEFS=vendor/lineage/sdclang/sdllvm-lto-defs.mk
+export SDCLANG_COMMON_FLAGS="-O3 -g0 -mcpu=krait -Wno-user-defined-warnings -Wno-vectorizer-no-neon -Wno-unknown-warning-option \
 -Wno-deprecated-register -Wno-tautological-type-limit-compare -Wno-sign-compare -Wno-gnu-folding-constant \
 -mllvm -arm-implicit-it=always -Wno-inline-asm -Wno-unused-command-line-argument -Wno-unused-variable"
 
