9367e75934a7dc98a6e22d9a4bc742c6917278cf
iusmac <iusico.maxim@libero.it>
2025-04-01 12:40:22 +0800

    config: Update DEX preopt boot image profile location
    They are now moved from {fwb}/config to {fwb}/boot and split into to
    files.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- config/common.mk
+++ config/common.mk
@@ -152,7 +152,9 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 # Use a profile based boot image for this device. Note that this is currently a
 # generic profile and not Android Go optimized.
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := \
+    frameworks/base/boot/boot-image-profile.txt \
+    frameworks/base/boot/boot-image-profile-extra.txt
 
 # Dedupe VNDK libraries with identical core variants.
 TARGET_VNDK_USE_CORE_VARIANT := true
