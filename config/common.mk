a9e12b6ea6735e88a3c759275fcf09705ac880e6
minaripenguin <minaripenguin@users.noreply.github.com>
2025-05-02 15:31:18 +0200

    Disable memtag for different processes
    * it defaults to async if not specified
    
    Change-Id: Ib724faf0a1510d73314931349245508287689311
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -301,9 +301,12 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.egl.blobcache.multifile=true \
     ro.egl.blobcache.multifile_limit=33554432
 
-# Disable async MTE on system_server
+# Disable async MTE on a few processes
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    arm64.memtag.process.system_server=off
+    persist.arm64.memtag.app.com.android.se=off \
+    persist.arm64.memtag.app.com.google.android.bluetooth=off \
+    persist.arm64.memtag.app.com.android.nfc=off \
+    persist.arm64.memtag.process.system_server=off
 
 include vendor/lineage/config/version.mk
 
