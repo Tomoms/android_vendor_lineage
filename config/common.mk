2c0ed3ac79f7375cf67f27d7ca9e3c6ef99cb294
Tommaso Fonda <fondatommaso2@gmail.com>
2025-10-22 14:23:52 +0200

    build Cromite
    Change-Id: I9739910167e389960256d4cf1e46466d76e72bf6

--- config/common.mk
+++ config/common.mk
@@ -141,7 +141,8 @@ PRODUCT_PACKAGES += \
 ifeq ($(PRODUCT_IS_ATV),)
 PRODUCT_PACKAGES += \
     ExactCalculator \
-    Jelly
+    Jelly \
+    CromiteWebView
 endif
 
 ifeq ($(PRODUCT_IS_AUTOMOTIVE),)
